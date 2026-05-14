69a0b4077aa07c08b581848ef0b72981d71d7c01
Koffi-akp01 <koffipenuelafola@gmail.com>
2026-05-14 20:06:14 +0000

    sauvegarde des ajouts, modifications et tout

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -22,9 +22,24 @@ export class LoginComponent {
     this.errorMessage = '';
 
     this.authService.login(this.credentials).subscribe({
-      next: () => {
-        const role = localStorage.getItem('user_role');
-        this.redirectByUserRole(role);
+      next: (response: any) => {
+        console.log('Réponse du serveur:', response); 
+        
+        if (response.access) {
+          localStorage.setItem('access_token', response.access);
+        }
+      
+        const role = response.role || localStorage.getItem('user_role');
+        
+        console.log('Rôle détecté:', role);
+
+        if (role) {
+          this.redirectByUserRole(role);
+        } else {
+          
+          console.warn("Aucun rôle trouvé, redirection par défaut vers étudiant");
+          this.router.navigate(['/dashboard-etudiant']); 
+        }
       },
       error: (err) => {
         console.error('Erreur de connexion', err);
